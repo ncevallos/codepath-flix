f383fa657de78496b9ce40dad0ac11b9ecf7901d
Natasha Cevallos <nmcevall@gmail.com>
2022-03-04 19:37:51 -0500

    completed part two
    Completed part two of the flix assignment which includes the superhero view and the detail movie view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Natasha Cevallos on 3/3/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+  
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
